24b7c5b10c9a437ba47a2b4c098cb6c32fcefd0d
amellalalex <github@alexamellal.com>
2022-05-26 13:32:49 -0300

    adjusted test to match new struct

--- tests/test.rs
+++ tests/test.rs
@@ -5,6 +5,7 @@ fn load_module() {
     let test = Module::open("tests/test.json").unwrap();
     let expected = Module::create(
         "test module",
+        "modules/acs",
         "./run_test_module.sh",
         vec!["arg1", "arg2", "arg3"],
     );
