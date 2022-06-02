36ea75ad95cff57f09b2c1ce79131e7fe0736d69
amellalalex <github@alexamellal.com>
2022-06-01 22:20:36 -0300

    using collection to load modules from folder

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use dockr::Collection;
+use dockr::Module;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     let mut coll = Collection::open_dir("modules")?;
+
     coll.run_all()?;
 
     Ok(())
